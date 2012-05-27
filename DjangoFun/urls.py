7c5050f7d108125a2c03b4468b4facb1858d55a5
Justin Beckwith <justin.beckwith@gmail.com>
2012-05-26 23:17:04 -0700

    commit after first run

--- DjangoFun/urls.py
+++ DjangoFun/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+# I added a comment to this file
 
 urlpatterns = patterns('',
     # Examples:
